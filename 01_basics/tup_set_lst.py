ee538c4698341e89495039f11037e36275e1d7ff
bsparrow49 <aashishchaudhari249@gmail.com>
2026-02-19 13:19:57 +0545

    collections

---
+++ 01_basics/tup_set_lst.py
@@ -0,0 +1,11 @@
+# collection = single "variable" used to convert to store multiple values
+# List = [] ordered and changeable, accepts duplicates
+# Set = {} unordered and immutable, allow Add/Remove but not duplicates
+# Tuple = () ordered and unchangeable, allow duplicates and is faster 
+fruits=["apple","banana","orange","coconut"]
+print(fruits)
+for fruits in fruits:
+    print(fruits)
+ 
+#  print(dir(fruits))
+print(help(fruits))
