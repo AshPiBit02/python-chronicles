7642adef690546df375a275da6fe6f599c8377e8
bsparrow49 <aashishchaudhari249@gmail.com>
2026-02-19 11:54:36 +0545

    basic cal

---
+++ 01_basics/calculator.py
@@ -0,0 +1,12 @@
+# python calculator
+operator=input("Enter an operator(+, -, /, *): ")
+num1=float(input("Enter the first number: "))
+num2=float(input("Enter the second number: "))
+if operator== "+":
+    print(num1+num2)
+elif operator=="*":
+    print(num1*num2)
+elif operator=="-":
+    print(num1-num2)
+elif operator=="/":
+    print(num1/num2)
