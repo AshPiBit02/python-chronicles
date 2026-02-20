ab438b9e95bf1d5436623b6859097b8928b9c9cd
bsparrow49 <aashishchaudhari249@gmail.com>
2026-02-20 12:04:24 +0545

    dict-imple-chlg01

--- 01_basics/Containers/dict.py
+++ 01_basics/Containers/dict.py
@@ -1,34 +1,57 @@
-# Dictionary
-# a collection of key_value pairs
-# Python 3.7+ preserve insertion order
-# uses a highly optimized hash table
-# use case: Fast lookups,representing JSON-like data, or mapping unique keys to values.
+# # Dictionary
+# # a collection of key_value pairs
+# # equivalent to unordered_map (in c++)
+# # Python 3.7+ preserve insertion order
+# # uses a highly optimized hash table
+# # use case: Fast lookups,representing JSON-like data, or mapping unique keys to values.
 
-#initialization
-my_dict={} #Empty dict if any_name{} 
-print(type(my_dict))
-my_dict={"name":"chdou","age":21} # standard literal 
-print(my_dict)
-my_dict=dict(name="Chdou",age=21) # constructor syntax
-print(my_dict)
+# #initialization
+# my_dict={} #Empty dict if any_name{} 
+# print(type(my_dict))
+# my_dict={"name":"chdou","age":21} # standard literal 
+# print(my_dict)
+# my_dict=dict(name="Chdou",age=21) # constructor syntax
+# print(my_dict)
 
-# Accessing
-val=my_dict["name"] # standard access(throws keyError if missing)
+# # Accessing
+# val=my_dict["name"] # standard access(throws keyError if missing)
+# print(val)
+# val=my_dict.get("status","default") # safe access with default value
+# print(val)
+
+# # Updates 
+# my_dict["city"]="NY" # insert/Update
+# my_dict.update({"age":3,"job":"IA"}) # bulk update
+
+# print(my_dict)
+
+# # Iteration
+# keys=my_dict.keys() # view of keys
+# print(keys)
+# values=my_dict.values() # veiw of values
+# print(values)
+# for k,v in my_dict.items(): # iterating over pairs
+#     print(k,v)
+
+# Challenge 1 (baiscs)
+my_dict={"name":"ALice","age":"25","is_admin":"True"}
+# 1. Access the "name" value
+val=my_dict["name"]
 print(val)
-val=my_dict.get("status","default") # safe access with default value
-print(val)
-
-# Updates 
-my_dict["city"]="NY" # insert/Update
-my_dict.update({"age":3,"job":"IA"}) # bulk update
-
+# 2. Update the "age" to 26
+my_dict["age"]=26
 print(my_dict)
-
-# Iteration
-keys=my_dict.keys() # view of keys
-print(keys)
-values=my_dict.values() # veiw of values
-print(values)
-for k,v in my_dict.items(): # iterating over pairs
+# 3. Add a new key-value pair: "email":"alice@gmail.com"
+my_dict.update({"email":"alice@gmail.com"})
+print(my_dict)
+for k,v in my_dict.items():
     print(k,v)
+# 4. Use the .get() method to look for a key called "phone".
+val=my_dict.get("phone","not found!") # if we try to access a key that is no present in list it will return the default value
+                                      # but we use my_dict["phone"] it will generate error as phone is not present so, .get() method is safe
+print(val)
+
+
+
+  
 
