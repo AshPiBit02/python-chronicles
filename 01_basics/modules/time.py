b15bf8cb84a41aa713de8c351bea49ddaf370813
bsparrow49 <aashishchaudhari249@gmail.com>
2026-02-21 10:12:15 +0545

    time-mod-ch-03-04

--- 01_basics/modules/time.py
+++ 01_basics/modules/time.py
@@ -33,21 +33,50 @@
 # elapses=end_time-start_time
 # print(f"Elpased time: {elapses:.2f} seconds") 
 
-# Ch 2 (countdown timer)
-    # Input: number of seconds 
-    # print countdown second by second
-    # End with "Time's up!"
-import time
-sec=int(input("Enter countdown time in  seconds: "))
-print("Countdown starts now!")
-# for i in range(sec,0,-1):
-#     print(i)
+# # Ch 2 (countdown timer)
+# #     Input: number of seconds 
+# #     print countdown second by second
+# #     End with "Time's up!"
+# import time
+# sec=int(input("Enter countdown time in  seconds: "))
+# print("Countdown starts now!")
+# # for i in range(sec,0,-1):
+# #     print(i)
+# #     time.sleep(1)
+# while sec>0:
+#     print(sec)
 #     time.sleep(1)
-while sec>0:
-    print(sec)
-    time.sleep(1)
-    sec-=1
-print("Time's up!")
+#     sec-=1
+# print("Time's up!")
+
+# # Challenge 3 (Execution time measurement)
+#   # Write a function that sums numbers from 1 to 1,000,000
+#   # measure how long it takes using time.pref.counter()
+# import time as tm
+# def sum_nums(n):
+#     total_sum=0
+#     for i in range(1,n+1):
+#         total_sum+=i
+#     return total_sum
+# start=tm.perf_counter()
+# result=sum_nums(1000000)
+# end=tm.perf_counter()
+# print("Sum: ",result)
+# print(f"Execition time: {end-start:.6f} seconds")
+
+# # Ch 4 (Digital clock)
+#    # Continuously print current time in HH:MM:SS
+#    # Update every second.
+# import time as tm
+# print("Digital Clock(press Ctrl+C to stop)") # ctrl+c is universal way to stop a python program(sends a KeyboardInterrupt)
+# while True:
+#     current_time=tm.strftime("%H:%M:%S",tm.localtime())
+#     print(current_time,end="\r") # overwrite the same line
+#     tm.sleep(1)
+
+
+
+
 
 
 
