d092d20f1125d409983a64ac9c2b6475f12ed82d
bsparrow49 <aashishchaudhari249@gmail.com>
2026-02-23 20:07:18 +0545

    random-mod

---
+++ 01_basics/modules/ran_dom.py
@@ -0,0 +1,37 @@
+# "random" (generating random numbers, choice and shuffling)
+     # very hardy for simulations, games, testing, and data sampling.
+
+# Core function in random
+import random as rdm
+
+# Random float(0.0<=x<1.0)
+print(rdm.random())
+
+# Random integer in a range
+print(rdm.randint(1,10)) # inclusive of both ends
+
+# Random Choice from a list
+fruits=["apple","banana","cherry","Orange","Mango"]
+print(rdm.choice(fruits))
+
+# Random Sample(Unique Picks)
+numbers=[1,2,3,3,3,4,5]
+print(rdm.sample(numbers,3)) # picks 3 random values 
+
+# Shuffle a List
+cards=["A","K","Q","J"]
+rdm.shuffle(cards)
+print(cards)
+
+# Uniform Distribution
+print(rdm.uniform(1,5)) # float between 1 and 5
+
+# Seed(Reproducible Randomness)
+rdm.seed(42) # not fully random but calculated is generated based on seed value
+print(rdm.randint(1,10))
+
+# number guessing game
+num=rdm.randint(1,100)
+
+
+
