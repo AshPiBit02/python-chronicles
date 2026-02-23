bee473d56d8d88078e6e2375f9aaa24f5bfbde8e
bsparrow49 <aashishchaudhari249@gmail.com>
2026-02-23 12:12:09 +0545

    args&kwargs-imple

--- 01_basics/Functions/variable_len_aug.py
+++ 01_basics/Functions/variable_len_aug.py
@@ -6,7 +6,7 @@
      # collects extra keyword arguments into a dictionary.
      # useful when you want flexible named parameters.
 
-# Examples
+# Examples(note: if you don't return anything while calling a function python automatically returns "None" as output when you call the function inside print() to wrap this use print inside function and just call from outside )
   
 #using *args
 def total(*args):
@@ -18,3 +18,36 @@ def show(**kwargs):
     for roll,name in kwargs.items():
         print(f"{roll}:{name}")
 print(show(roll=1,name="Aashish"))
+
+# Write a dunction multiply_all(*args) that multiplies all numbers passed.
+def multiply_all(*args):
+    prod=1
+    for num in args:
+        prod*=num
+    return prod
+print(multiply_all(1,2,3,4))
+
+#Write a function student_info(**kwargs) that prints studnet details.
+def student_info(**kwargs):
+    for i,j in kwargs.items():
+        print(f"Details {i}:{j}")
+print(student_info(Name="Aashish",roll=2,Semster="Third"))
+
+#Write a function mix(*args,**kwargs) that prints both positional and keyword arguments.
+def mix(*args,**kwargs):
+    for i in args:
+        print(i)
+    for i,j in kwargs.items():
+        print(f"{i} : {j} ")
+print(mix(1,2,3,4,name="Abdt",level="Bachelor"))
+
+# Write a function average(*args) that returns the average of numbers.
+def average(*args):
+    print(f"Average is {sum(args)/len(args)}")
+average(1,2,3,4,5,6)
+
+# Write a function config(**kwargs) that prints system configuration details.
+def config(**kwargs):
+    for key,value in kwargs.items():
+        print(f"{key}: {value}")
+config(OS="Windows",version="15.1",RAM="32GB",CPU="Intel X3",GPU="Nvidia RTX 5090Ti")
