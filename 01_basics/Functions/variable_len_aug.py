d112a776fc8532a23a6f3623fc405d1b41dc512f
bsparrow49 <aashishchaudhari249@gmail.com>
2026-02-23 12:41:50 +0545

    args&kwargs-imple done

--- 01_basics/Functions/variable_len_aug.py
+++ 01_basics/Functions/variable_len_aug.py
@@ -17,7 +17,7 @@ print(total(1,2,3,4))
 def show(**kwargs):
     for roll,name in kwargs.items():
         print(f"{roll}:{name}")
-print(show(roll=1,name="Aashish"))
+show(roll=1,name="Aashish")
 
 # Write a dunction multiply_all(*args) that multiplies all numbers passed.
 def multiply_all(*args):
@@ -31,7 +31,7 @@ print(multiply_all(1,2,3,4))
 def student_info(**kwargs):
     for i,j in kwargs.items():
         print(f"Details {i}:{j}")
-print(student_info(Name="Aashish",roll=2,Semster="Third"))
+student_info(Name="Aashish",roll=2,Semster="Third")
 
 #Write a function mix(*args,**kwargs) that prints both positional and keyword arguments.
 def mix(*args,**kwargs):
@@ -39,7 +39,7 @@ def mix(*args,**kwargs):
         print(i)
     for i,j in kwargs.items():
         print(f"{i} : {j} ")
-print(mix(1,2,3,4,name="Abdt",level="Bachelor"))
+mix(1,2,3,4,name="Abdt",level="Bachelor")
 
 # Write a function average(*args) that returns the average of numbers.
 def average(*args):
@@ -51,3 +51,68 @@ def config(**kwargs):
     for key,value in kwargs.items():
         print(f"{key}: {value}")
 config(OS="Windows",version="15.1",RAM="32GB",CPU="Intel X3",GPU="Nvidia RTX 5090Ti")
+
+#Write a function profile(*args,**kwargs) that prints positional arguments as hobbies and keyword arguments as personal details
+def profile(*args,**kwargs):
+    print("Hobbies:")
+    print("-"*40)
+    for i in args:
+        print(i)
+    print("Personal Details:")
+    print("-"*40)
+    for i,j in kwargs.items():
+        print(f"{i}: {j}")
+profile("Coding","Developing","Learning","Riding","Implementing","Thinking",Name="Aashish Chaudhary",Phone_no="9746666800",Address="Nepal",Education="Undergraduate")
+
+#Write a function report(*args,**kwargs) that prints numbers passed in *args and metadata in **kwargs.
+def report(*args,**kwargs):
+    print("Report numbers:")
+    print("-"*40)
+    if args:
+        print("Numbers: ",args)
+    else:
+        print("No numbers provided.")
+    print("Metadata")
+    print("-"*40)
+    if kwargs:
+        for k,v in kwargs.items():
+            print(f"{k}: {v}")
+    else:
+        print("No meta provided.")
+    print("-"*40)
+report(101,102,104,author="Aashish",subject="Data Structure",semester="Thirds")      
+
+# Write a function mix_data(*args,**kwargs) that separates positional and keyword arguments clearly.
+def mix(*args,**kwargs):
+    print("\nPositional Arguments:")
+    print("-"*40)
+    if args:
+        for i in args:
+            print(i)
+    else:
+        print("No positional arguments.")
+    print("\nKeyword Arguments:")
+    print("-"*40)
+    if kwargs:
+        for i,j in kwargs.items():
+            print(f"{i}: {j}")
+    else:
+        print("No keyword arguments")
+mix("DjBravo","Mjeik","Mujnor","Tdki",Stage="The Tokyo",Nao="Juntu",Densn="Tems")
+mix(Stage="The Tokyo",Nao="Juntu",Densn="Tems")
+mix("DjBravo","Mjeik","Mujnor","Tdki")
+
+#Write a function registers(*args,**kwargs) that accepts multiple IDs as *args and user details as **kwargs.
+def registers(*args,**kwargs):
+    print("\n\nRegister")
+    if args:
+        print("User IDs:")
+        print("-"*40)
+        for i in args:
+            print(i)
+    if kwargs:
+        print("\nUser details:")
+        print("-"*40)
+        for k,v in kwargs.items():
+            print(f"{k}: {v}")
+registers(102,104,103,101,501,Name="Funu",Department="Tdx",Salary="That equal")
