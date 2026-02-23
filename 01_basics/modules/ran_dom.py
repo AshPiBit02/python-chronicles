0562e2da076ef136a8c121b308ae57a107043104
bsparrow49 <aashishchaudhari249@gmail.com>
2026-02-23 20:19:18 +0545

    num-gues-gm

--- 01_basics/modules/ran_dom.py
+++ 01_basics/modules/ran_dom.py
@@ -27,11 +27,26 @@ print(cards)
 print(rdm.uniform(1,5)) # float between 1 and 5
 
 # Seed(Reproducible Randomness)
-rdm.seed(42) # not fully random but calculated is generated based on seed value
+# rdm.seed(42) # not fully random but calculated is generated based on seed value
 print(rdm.randint(1,10))
 
 # number guessing game
+print("Number Guessing Game")
+print("-"*35)
 num=rdm.randint(1,100)
+in_num=int(input("Guess the number: "))
+count=0
+while(in_num!=num):
+    if(in_num>num):
+        print("Input is High!")
+    elif (in_num<num):
+        print("Input is Low!")
+    in_num=int(input("Enter next Guess: "))
+    count+=1
+print(f"Correct guess in {count} try")
+
+
+    
 
 
 
