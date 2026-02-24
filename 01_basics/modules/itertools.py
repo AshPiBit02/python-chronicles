84cb763515f6d9abdd6cd692181ef22c7553c0e3
bsparrow49 <aashishchaudhari249@gmail.com>
2026-02-24 19:24:36 +0545

    itertools-intro-func

---
+++ 01_basics/modules/itertools.py
@@ -0,0 +1,40 @@
+# "itertools" a standard library module fro iterator building blocks.
+    # allows to create fast, memory-efficient tools for looping, counting, grouping, and generating combinations.
+    # often used in data science, algorithms, and combinatorics.
+# Key Functions in "itertools"
+
+# 1. Infinite Iterators 
+    # count(start=0,step=1) - infinite counting sequence.
+from itertools import count
+for i in count(10,2):
+    if i>20: break
+    print(i)
+   
+   # cycle(iterable) - repeats elements forever.
+from itertools import cycle
+c=cycle("AB")
+for i in range(5):
+    print(next(c))
+
+   # repeat(object,times=None) - repeats an object
+from itertools import repeat
+for x in repeat("Hi",3):
+    print(x)
+
+# 2. Iterators Terminating on Shortest Input 
+   # accumulate(iterable,func=operator.add) - running totals.
+from itertools import accumulate
+import operator
+print(list(accumulate([1,2,3,4],operator.mul)))
+   # chain(*iterables) - links multiple iterables together
+from itertools import chain
+print(list(chain([1,2],['a','b'])))
+   # compress(data,selectors) - filters data by selectors.
+from itertools import compress
+print(list(compress("ABCDEF",[1,1,0,1,0,0j])))
+   # dropwhile(predicate,iterable) - drop items untill predicate is False
+from itertools import dropwhile
+print(list(dropwhile(lambda x:x<5,[1,2,3,12,6,5])))
+   # takewhile(predicate,iterable) - take items while pedicate is True
+from itertools import takewhile
+print(list(takewhile(lambda x:x%2==0,[12,24,20,11,20,24])))
