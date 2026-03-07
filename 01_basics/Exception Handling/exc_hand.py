6a8c5b19860df5cbba366f1cb2f169c0439b6a8d
bsparrow49 <aashishchaudhari249@gmail.com>
2026-03-07 21:13:05 +0545

    intro to exception handling

---
+++ 01_basics/Exception Handling/exc_hand.py
@@ -0,0 +1,15 @@
+# Exception handling in Python is a way to manage runtime errors gracefully.
+# Usage :
+   # Prevent crashes: handle errors without stopping the program.
+   # Provide feedback: Show user-friendly messages.
+   # Ensure cleanup: close files, release resources, or reset states.
+   # Control flow: Decide what happens next when something goes wrong.
+# Syntax:  
+    # try:
+       # code that may cause an error
+    # except ExceptionType:
+       # code to handle the error
+    # else:
+       # code that runs if no error occurs
+    # finally: 
+       # code that always runs (cleanup)
