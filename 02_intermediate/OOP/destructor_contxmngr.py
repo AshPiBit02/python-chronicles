0387f9b915a2f418a6738f9577c8b448dc48fefe
bsparrow49 <aashishchaudhari249@gmail.com>
2026-03-10 20:59:16 +0545

    destructor practical example

--- 02_intermediate/OOP/destructor_contxmngr.py
+++ 02_intermediate/OOP/destructor_contxmngr.py
@@ -24,3 +24,16 @@ class Demo:
         print(f"Destructor called, object {self.name} deleted")
 d=Demo("test")
 del d # delete object explicitly
+
+   # File handling
+class FileHandler:
+    def __init__(self,filename):
+        self.file=open(filename,"w")
+        print("File opened")
+    def write(self,text):
+        self.file.write(text)
+    def __del__(self):
+        self.file.close()
+        print("File closed automatically")
+f=FileHandler("demo.txt")
+f.write("This is a demo file")
