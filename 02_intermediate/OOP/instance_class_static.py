d3e563348843fa2fbe7e379dd226fef7b8eeac5f
bsparrow49 <aashishchaudhari249@gmail.com>
2026-03-11 21:43:35 +0545

    hands-on proj on instance method init(lvl 1)

--- 02_intermediate/OOP/instance_class_static.py
+++ 02_intermediate/OOP/instance_class_static.py
@@ -207,3 +207,31 @@ except ValueError as e:
     print(f"Error: {e}")
 print(ac.balance)
 
+# Hands-On projects using instance method
+
+# Level 1(Attribute Basics)
+   # Create a 'Pen' class with attributes 'color' and 'ink_levl'.
+   # Write an instance method write(text) that reduces ink level by 1 each time it's called.
+   # Add another method refill(amount) that increases ink level.
+class Pen:
+    def __init__(self,color="Black",ink_level=3):
+        self.color=color
+        self.ink_level=ink_level
+    def write(self):
+        if self.ink_level==0:
+            print("Can't write(no ink left)")
+            return 
+        # writes some data
+        print("write done")
+        self.ink_level-=1
+    def refill(self,amount):
+        self.ink_level+=amount
+p=Pen()
+p.write()
+p.write()
+p.write()
+p.write()
+p.refill(2)
+p.write()
+p.write()
+
