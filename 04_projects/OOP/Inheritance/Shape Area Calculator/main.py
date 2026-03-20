15e6402bf5f7a33db8a3190af228a0b5bed90967
bsparrow49 <aashishchaudhari249@gmail.com>
2026-03-20 21:46:46 +0545

    feat(Areas) -> returns the area of shapes

---
+++ 04_projects/OOP/Inheritance/Shape Area Calculator/main.py
@@ -0,0 +1,10 @@
+from shapes import Circle, Rectangle, Triangle
+def main():
+    cir=Circle(5)
+    rect=Rectangle(2,3)
+    tri=Triangle(6,2)
+    print(cir)
+    print(rect)
+    print(tri)
+if __name__=="__main__":
+    main()
