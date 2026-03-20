8a2f39f234abf284a5d85e6aeb7539665f61f1b5
bsparrow49 <aashishchaudhari249@gmail.com>
2026-03-20 19:59:21 +0545

    cls(full,part-time) -> basic class structures

--- 04_projects/OOP/Inheritance/employee_payroll/models.py
+++ 04_projects/OOP/Inheritance/employee_payroll/models.py
@@ -8,4 +8,17 @@ class Employee:
     def __str__(self):
         return f"{self.name} earns {self.calculate_salary()}$ per month"
 class FullTimeEmployee(Employee):
-    def __init__(self,name,base,base_salary,bonus=0)
+    def __init__(self,name,base_salary,bonus=0):
+        super().__init__(name,base_salary)
+        self.bonus=bonus
+    def calculate_salary(self):
+        """Full-time employees get base + bonus"""
+        return super().calculate_salary() + self.bonus
+class PartTimeEmployee(Employee):
+    def __init__(self,name,hourly_rate,hours_worked):
+        """Part-time employees don't use base_salary"""
+        super().__init__(name,base_salary=0)
+        self.hourly_rate=hourly_rate
+        self.hours_worked=hours_worked
+    def calculate_salary(self):
+        return self.hourly_rate * self.hours_worked
