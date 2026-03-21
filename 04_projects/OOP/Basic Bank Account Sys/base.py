cab84ff48d26249770322d10018d865de20bcd2e
bsparrow49 <aashishchaudhari249@gmail.com>
2026-03-21 17:36:34 +0545

    feat(withdraw) -> deducts valid amounts

--- 04_projects/OOP/Basic Bank Account Sys/base.py
+++ 04_projects/OOP/Basic Bank Account Sys/base.py
@@ -7,5 +7,12 @@ class Account:
         if not isinstance(amount,(int,float)) or (amount <= 0):
             raise ValueError("Invalid Amount!")
         self.__balance+= amount
+    def withdraw(self,amount):
+        if not isinstance(amount,(int,float)) or amount <= 0:
+            raise ValueError("Invlaid Amount!")
+        if self.__balance < amount:
+            raise ValueError("Insufficient Balance!")
+        self.__balance-=amount
+
         
 
