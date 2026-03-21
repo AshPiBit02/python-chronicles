bcfc78682feff81d950b945d7e50eac33af75cf9
bsparrow49 <aashishchaudhari249@gmail.com>
2026-03-21 17:19:14 +0545

    init proj(basic bank system) -> cls(account)

---
+++ 04_projects/OOP/Basic Bank Account Sys/base.py
@@ -0,0 +1,8 @@
+class Account:
+    def __int__(self,account_holder,account_number,balance):
+        self.account_holder=account_holder
+        self.account_number=account_number
+        self.__balance=balance
+
+        
+
