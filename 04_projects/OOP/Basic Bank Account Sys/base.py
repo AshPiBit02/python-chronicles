1a529d9d654cdd802ab30ff7b85d04eb80955a83
bsparrow49 <aashishchaudhari249@gmail.com>
2026-03-21 17:34:06 +0545

    feat(deposit) -> add valid amounts

--- 04_projects/OOP/Basic Bank Account Sys/base.py
+++ 04_projects/OOP/Basic Bank Account Sys/base.py
@@ -1,8 +1,11 @@
 class Account:
-    def __int__(self,account_holder,account_number,balance):
+    def __init__(self,account_holder,account_number,balance):
         self.account_holder=account_holder
         self.account_number=account_number
         self.__balance=balance
-
+    def deposit(self,amount):
+        if not isinstance(amount,(int,float)) or (amount <= 0):
+            raise ValueError("Invalid Amount!")
+        self.__balance+= amount
         
 
