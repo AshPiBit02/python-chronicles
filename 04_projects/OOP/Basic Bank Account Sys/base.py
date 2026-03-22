72f050b41d24a87fe8f064376cbc0d45e4bd9a6a
bsparrow49 <aashishchaudhari249@gmail.com>
2026-03-22 21:44:45 +0545

    feat(overdraft_limit) -> allows to withdrawn certain amounts even if there is no balance

--- 04_projects/OOP/Basic Bank Account Sys/base.py
+++ 04_projects/OOP/Basic Bank Account Sys/base.py
@@ -17,8 +17,8 @@ class Account:
         return self.__balance
 class SavingAccount(Account):
     def __init__(self,account_holder,account_number,balance,interest_rate):
-        super.__init__(account_holder,account_number,balance)
-        if not isinstance(self.interest_rate,(int,float)) or self.interest_rate < 0 :
+        super().__init__(account_holder,account_number,balance)
+        if not isinstance(interest_rate,(int,float)) or self.interest_rate < 0 :
             raise ValueError("Invalid Interest Rate!")
         self.interest_rate=interest_rate
 
@@ -26,6 +26,19 @@ class SavingAccount(Account):
         balance=self.get_balance()
         interest_amt=(self.interest_rate/100)*balance
         self.deposit(interest_amt)
+class CurrentAccount(Account):
+    def __init__(self,account_holder,account_number,balance,overdraft_limit=0.0):
+        super().__init__(account_holder,account_number,balance)
+        self.overdraft_limit=float(overdraft_limit)
+    def withdraw(self, amount):
+        if not isinstance(amount,(int,float)) or amount <=0:
+            raise ValueError("Invalid amount!")
+        current=self.get_balance()
+        if amount > current + self.overdraft_limit:
+            raise ValueError("Exceeds overdraft limit!")
+        self.__balance-=amount
+      
+
 
 
         
