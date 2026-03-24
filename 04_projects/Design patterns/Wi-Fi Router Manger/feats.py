b6b025f9fa11ce3825e169ae342cbc3704c65aaf
bsparrow49 <aashishchaudhari249@gmail.com>
2026-03-24 21:11:04 +0545

    proj(wifi-router-manager) -> alignment

--- 04_projects/Design patterns/Wi-Fi Router Manger/feats.py
+++ 04_projects/Design patterns/Wi-Fi Router Manger/feats.py
@@ -20,5 +20,13 @@ class Router:
         print(f"Devices connected to {self.ssid}:")
         i=1
         for item in self.list:
-            print(f:<100"{i}. {item}")
+            line=f"{i}. {item}"
+            print(f"{' '*25}{line}")
             i+=1
+router1=Router("FiberNet","Nep@L57")
+router1.connect_device("Honor 300 Lite")
+router1.connect_device("Samsung S23 Ultra")
+router1.connect_device("Iphone 17 Pro")
+router1.connect_device("Iphone 17 Pro")
+router1.connect_device("ZenBook 14")
+router1.show_connected_devices()
