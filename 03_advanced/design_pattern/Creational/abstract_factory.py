821d56202e9c666d9d5604995b1be62c044e980e
bsparrow49 <aashishchaudhari249@gmail.com>
2026-03-25 21:24:55 +0545

    eg(Database connector) -> abstract factory (sql vs nosql)

--- 03_advanced/design_pattern/Creational/abstract_factory.py
+++ 03_advanced/design_pattern/Creational/abstract_factory.py
@@ -16,3 +16,66 @@ Abstract Factory Pattern
   -> Database connectors(e.g. Factories for SQL vs NoSQL families of objects.)
 
 """
+# Example
+""" DataBase Connector(SQL vs NoSQL) """
+
+# Abstract Products
+class QueryBuilder:
+    def build_query(self):
+        raise NotImplementedError
+class TransactionManager:
+    def manage_transaction(self):
+        raise NotImplementedError
+
+# Concrete Products for SQL
+class SQLQueryBuilder(QueryBuilder):
+    def build_query(self):
+        print("Building SQL query: SELECT * FROM users")
+    
+class SQLTransactionManager(TransactionManager):
+    def manage_transaction(self):
+        print("Managing SQL transaction")
+
+# Concret Products for NoSQL
+class NoSQLQueryBuilder(QueryBuilder):
+    def build_query(self):
+        print("Building NoSQL query: { find: 'users'}")
+
+class NoSQLTransactionManager(TransactionManager):
+    def manage_transaction(self):
+        print("Managing NoSQL transaction")
+
+# Abstract Factory:
+class DatabaseFactory:
+    def create_query_builder(self):
+        raise NotImplementedError
+    def create_transaction_manager(self):
+        raise NotImplementedError
+    
+# Concrete Factories
+class SQLFactory(DatabaseFactory):
+    def create_query_builder(self):
+        return SQLQueryBuilder()
+    def create_transaction_manager(self):
+        return SQLTransactionManager()
+    
+class NoSQLFactory(DatabaseFactory):
+    def create_query_builder(self):
+        return NoSQLQueryBuilder()
+    def create_transaction_manager(self):
+        return NoSQLTransactionManager()
+
+# Client Code
+def client_code(factory: DatabaseFactory):
+    qb=factory.create_query_builder()
+    tm=factory.create_transaction_manager()
+    qb.build_query()
+    tm.manage_transaction()
+
+# Test with SQL
+print(f"{'-'*7} SQL DataBase {'-'*7}")
+client_code(SQLFactory())
+
+# Test with NoSQL
+print(f"{'-'*7} NoSQL DataBase {'-'*7}")
+client_code(NoSQLFactory())
