a6da3aff52ebe5a94e890440ff8021e019836cb5
bsparrow49 <aashishchaudhari249@gmail.com>
2026-03-25 15:22:54 +0545

    state after merge with test_projs

--- 04_projects/Design patterns/Wi-Fi Router Manger/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-y:
-#     router1.change_ssid()
-# except ValueError as e:
-#     print("Error: ",e)
-
-# try:
-#     router1.update_password()
-# except ValueError as e:
-#     print("Error: ",e)
--- 04_projects/Design_patterns/Notification System/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-tifier("sms")
-# sms.send("Your OTP is 1474")
-# push=nf.get_notifier("push")
-# push.send("You have received 10 new messages","urgent")
-# try:
-#     uk=nf.get_notifier("unknown")
-#     uk.send("unknown message")
-# except ValueError as e:
-#     print("Error: ",e)
