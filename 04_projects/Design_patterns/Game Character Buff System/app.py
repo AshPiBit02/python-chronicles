b9239d2eedc675cc25e58511fe3d6cf7beeccba5
bsparrow49 <aashishchaudhari249@gmail.com>
2026-03-28 13:36:53 +0545

    feat(battle)

--- 04_projects/Design_patterns/Game Character Buff System/app.py
+++ 04_projects/Design_patterns/Game Character Buff System/app.py
@@ -14,6 +14,14 @@ class BaseCharacter:
             "Speed":self.speed
         }
     
+    def take_damange(self,amount):
+        self.health-=amount
+        if self.health < 0:
+            self.health =0
+    
+    def is_alive(self):
+        return self.health > 0
+    
 # Abstract Decorator
 class CharacterDecorator(BaseCharacter):
     def __init__(self,character):
@@ -21,6 +29,12 @@ class CharacterDecorator(BaseCharacter):
     
     def stats(self):
         return self.character.stats()
+    
+    def take_damange(self, amount):
+        self.character.take_damage(amount)
+
+    def is_alive(self):
+        return self.character.is_alive()
     
 # Concrete Decorator
 class SpeedBoost(CharacterDecorator):
@@ -40,3 +54,28 @@ class DoubleDamage(CharacterDecorator):
         base_stats=super().stats()
         base_stats["Attack"] *=2
         return base_stats
+
+# Battle Simulation
+def battle(char1,char2):
+    print(f"{'-'*7} Battle Start {'-'*7}")
+    print(char1.stats())
+    print(char2.stats())
+    print('-'*40)
+
+    while char1.is_alive() and char2.is_alive():
+        if char1.stats()["Speed"] >= char2.stats()["Speed"]:
+            attacker,defender=char1,char2
+        else:
+            attacker,defender=char2,char1
+    
+    # Attack
+    damage=attacker.start()["Attack"]
+    defender.take_damage(damage)
+    print(f"{attacker.stats()["Name"]} attacks {defender.starts()["Name"]} for {damage} damage!")
+
+    print(f"{defender.stats()["Name"]} health: {defender.stats()["Health"]}")
+    print('-'*35)
+    if not defender.is_alive():
+        print(f"{defender.stats()["Name"]} has been defeated!")
+        return
+    
--- 04_projects/Design_patterns/Game Character Buff System/main.py
+++ 04_projects/Design_patterns/Game Character Buff System/main.py
@@ -1,10 +1,21 @@
-from app import Shield,DoubleDamage,SpeedBoost,CharacterDecorator,BaseCharacter
+from app import Shield,DoubleDamage,SpeedBoost,CharacterDecorator,BaseCharacter,battle
 
 if __name__=="__main__":
     ch1=BaseCharacter("Knight",health=100,attack=30,speed=10)
 
-    buffed_ch1=DoubleDamage(SpeedBoost(Shield(ch1)))
+    buffed_ch1=Shield(DoubleDamage(SpeedBoost(Shield(ch1))))
     
     print("Final Buffed stats: ")
     for k,v in buffed_ch1.stats().items():
         print(f"{k}: {v}")
+
+
+
+    Knight1=BaseCharacter("Knight A",health=100,attack=30,speed=10)
+    Knight2=BaseCharacter("Knight B",health=120,attack=25,speed=15)
+
+    buffed_knight1=DoubleDamage(SpeedBoost(Shield(Knight1)))
+    buffed_knight2=Shield(Knight2)
+
+    battle(buffed_knight1,buffed_knight2)
+
