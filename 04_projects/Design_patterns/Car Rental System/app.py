cbc480c3ed74b61a85dc1722e7eb4c25c4385a54
bsparrow49 <aashishchaudhari249@gmail.com>
2026-03-29 13:34:38 +0545

    feat(add-ons) -> GPS,Insurance,ChildSeat,ExtraMileage

--- 04_projects/Design_patterns/Car Rental System/app.py
+++ 04_projects/Design_patterns/Car Rental System/app.py
@@ -10,13 +10,41 @@ class CarRental:
         return f"Car Mode: {self.car_model}\nRent Cost: {self.base_price}$\n Days: {self.duration}"
     
 # Abstract Decorator
-class RentalDecorator:
+class RentalDecorator(CarRental):
     def __init__(self,carRental):
         self.carRental=carRental
     def cost(self):
         return self.carRental.cost()
     def cost(self):
         return self.carRental.description()
+
+# Concrete Decorators
+class GPS(CarRental):
+    def cost(self):
+        return super().cost() + 500
+    def description(self):
+        return super().description() + "\nGPS Included"
+    
+class Insurance(CarRental):
+    def cost(self):
+        return super().cost() + 1000
+    def description(self):
+        return super().description() + "\nInsurance Included"
+    
+class ChildSeat(CarRental):
+    def cost(self):
+        return super().cost() + 300
+    def description(self):
+        return super().description() + "\nChild Seat Included"
+
+class ExtraMileage(CarRental):
+    def cost(self):
+        return super().cost() + 700
+    def description(self):
+        return super().description() + "\nExtra Mileage Included"
+    
+    
+
     
 
 
