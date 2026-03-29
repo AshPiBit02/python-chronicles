fd30818a4122b33d999c077ecb4255b20fc565ae
bsparrow49 <aashishchaudhari249@gmail.com>
2026-03-29 13:54:50 +0545

    feat(UI +description) -> enhanced client usage, fixed descriptio format

--- 04_projects/Design_patterns/Car Rental System/app.py
+++ 04_projects/Design_patterns/Car Rental System/app.py
@@ -23,25 +23,25 @@ class GPS(RentalDecorator):
     def cost(self):
         return super().cost() + 500
     def description(self):
-        return super().description() + "\nGPS Included"
+        return super().description() + "\nGPS Included (+500$)"
     
 class Insurance(RentalDecorator):
     def cost(self):
         return super().cost() + 1000
     def description(self):
-        return super().description() + "\nInsurance Included"
+        return super().description() + "\nInsurance Included (+1000$)"
     
 class ChildSeat(RentalDecorator):
     def cost(self):
         return super().cost() + 300
     def description(self):
-        return super().description() + "\nChild Seat Included"
+        return super().description() + "\nChild Seat Included (+300$)"
 
 class ExtraMileage(RentalDecorator):
     def cost(self):
         return super().cost() + 700
     def description(self):
-        return super().description() + "\nExtra Mileage Included"
+        return super().description() + "\nExtra Mileage Included (+700$)"
 
 
 
