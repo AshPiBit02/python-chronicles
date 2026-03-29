1dc2ce9e58fbd75d66b42e1ce38e2bb78051a7fe
bsparrow49 <aashishchaudhari249@gmail.com>
2026-03-29 13:49:36 +0545

    feat(client usage) -> proper format

--- 04_projects/Design_patterns/Car Rental System/user.py
+++ 04_projects/Design_patterns/Car Rental System/user.py
@@ -1,5 +1,12 @@
 from app import GPS,Insurance,ChildSeat,ExtraMileage,CarRental
 hilux=CarRental("Toyota Hilux",base_price=8000,duration=3)
+isuzu=CarRental("Isuzu D-max",base_price=9000,duration=2)
+
 rent=ChildSeat(hilux)
-print(rent.cost())
 print(rent.description())
+print(f"Total Cost: {rent.cost()}$")
+
+
+rent2=GPS(Insurance(ChildSeat(isuzu)))
+print(rent2.description())
+print(f"Total Cost: {rent2.cost()}$")
