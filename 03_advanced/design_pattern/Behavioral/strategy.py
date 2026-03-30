58fdaf9b5dac226a8b3b52003bd282039d155c11
bsparrow49 <aashishchaudhari249@gmail.com>
2026-03-30 11:25:50 +0545

    example(strategy pattern

--- 03_advanced/design_pattern/Behavioral/strategy.py
+++ 03_advanced/design_pattern/Behavioral/strategy.py
@@ -37,3 +37,33 @@ Strategy Pattern
     The 'Context' is "going to work". The Strategy is the chosen mode of transport. You can swap 
      strategies depending on traffic, weather, or cost.
 """
+
+# Example 2
+
+# Strategy Interface
+class PaymentStrategy:
+    def pay(self,amount):
+        pass
+    
+# Concrete Strategies
+class CreditCardPayment(PaymentStrategy):
+    def pay(self,amount):
+        print(f"Paid {amount}$ using Credit Card.")
+
+class PayPalPayment(PaymentStrategy):
+    def pay(self,amount):
+        print(f"Paid {amount}$ using PayPal.")
+
+# Context
+class ShoppingCart:
+    def __init__(self,payment_strategy:PaymentStrategy):
+        self.payment_strategy=payment_strategy
+    def checkout(self,amount):
+        self.payment_strategy.pay(amount)
+
+# Client Usage
+cart1=ShoppingCart(CreditCardPayment())
+cart1.checkout(211)
+
+cart2=ShoppingCart(PayPalPayment())
+cart2.checkout(21.03)
