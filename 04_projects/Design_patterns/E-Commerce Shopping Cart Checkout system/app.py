b76d774b824396d63a3b1be85ca044b5b0861f28
bsparrow49 <aashishchaudhari249@gmail.com>
2026-04-03 15:08:03 +0545

    bugs on app.py fixed , feat(handles out of stock products)

--- 04_projects/Design_patterns/E-Commerce Shopping Cart Checkout system/app.py
+++ 04_projects/Design_patterns/E-Commerce Shopping Cart Checkout system/app.py
@@ -8,6 +8,7 @@ class CheckoutReport:
         self.payment=0
         self.invoice=" "
         self.gift_message=None
+        self.out_of_stock=[]
     
     def show(self):
         print("Checkout Report")
@@ -18,6 +19,8 @@ class CheckoutReport:
         print(f"Payment: {self.payment}$")
         if self.gift_message:
             print(f"Gift Message: {self.gift_message}")
+        if self.out_of_stock:
+            print(f"Out of Stock: {','.join(self.out_of_stock)}")
         print(f"Invoice: {self.invoice}")
 
 # Builder Interface
@@ -34,16 +37,20 @@ class StandardCheckoutBuilder(CheckoutBuilder):
     def __init__(self):
         self.report=CheckoutReport()
     def add_items(self, items:list):
-        self.report.items.extend(items)
+        for item in items:
+            if item in items_cost:
+                self.report.items.append(item)
+            else:
+                self.report.out_of_stock.append(item)
     def apply_discount(self, discount_percentage: float):
-        subtotal=sum([items_cost[item] for item in self.report.items])
+        subtotal=sum([items_cost[item] for item in self.report.items if item in items_cost])
         self.report.discounts=subtotal * discount_percentage
     def add_shipping(self, shipping_cost: float):
         self.report.shipping=shipping_cost
     def process_payment(self, payment_amount: float):
         self.report.payment=payment_amount
     def generate_invoice(self, customer_name: str):
-        self.report.invoice=f"Invoice fro {customer_name}: {self.report.payment}$ paid."
+        self.report.invoice=f"Invoice for {customer_name}: {self.report.payment}$ paid."
     def get_result(self):
         return self.report
     
@@ -51,7 +58,11 @@ class ExpressCheckoutBuilder(CheckoutBuilder):
     def __init__(self):
         self.report=CheckoutReport()
     def add_items(self, items:list):
-        self.report.items.extend(items)
+        for item in items:
+            if item in items_cost:
+                self.report.items.append(item)
+            else:
+                self.report.out_of_stock.append(item)
     def apply_discount(self, discount_percentage: float):
         # Express checkout skips discount
         self.report.discounts=0
@@ -68,9 +79,13 @@ class GiftCheckoutBuilder(CheckoutBuilder):
     def __init__(self):
         self.report=CheckoutReport()
     def add_items(self, items:list):
-        self.report.items.extend(items)
+        for item in items:
+            if item in items_cost:
+                self.report.items.append(item)
+            else:
+                self.report.out_of_stock.append(item)
     def apply_discount(self, discount_percentage: float):
-        subtotal=sum([items_cost[item] for item in self.report.items])
+        subtotal=sum([items_cost[item] for item in self.report.items if item in items_cost])
         self.report.discounts=subtotal * discount_percentage
     def add_shipping(self, shipping_cost: float):
         self.report.shipping=shipping_cost
@@ -89,13 +104,23 @@ class Director:
         self.builder=builder
     def construct(self,items: list,discount_percentage:float,shipping_cost:float,payment_amount:float,customer_name:str,gift_message:str=None):
         self.builder.add_items(items)
-        self.builder.apply_discount(discount_percentage)
-        self.builder.add_shipping(shipping_cost)
-        self.builder.process_payment(payment_amount)
-        self.builder.generate_invoice(customer_name)
-        if isinstance(self.builder,GiftCheckoutBuilder) and gift_message:
-            self.builder.add_gift_wrap(gift_message)
-        return self.builder.get_result()
+        report=self.builder.get_result()
+        # Only continue if there are valid items
+        if report.items:
+            self.builder.apply_discount(discount_percentage)
+            self.builder.add_shipping(shipping_cost)
+            self.builder.process_payment(payment_amount)
+            self.builder.generate_invoice(customer_name)
+            if isinstance(self.builder,GiftCheckoutBuilder) and gift_message:
+                self.builder.add_gift_wrap(gift_message)
+            return self.builder.get_result()
+        else:
+            # No items -> mark invoice accrodingly
+            report.shipping=0
+            report.payment=0
+            report.invoice=f"No items in cart for {customer_name}"
+        
+        return report
     
         
     
