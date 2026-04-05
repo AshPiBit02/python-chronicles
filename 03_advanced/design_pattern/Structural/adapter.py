1d7750370cacd11f3f3c679893d7f6cea549752b
bsparrow49 <aashishchaudhari249@gmail.com>
2026-04-05 22:07:19 +0545

    init(adapter)

---
+++ 03_advanced/design_pattern/Structural/adapter.py
@@ -0,0 +1,28 @@
+"""
+Adapater Pattern
+ -> a structural design pattern that allows two incompatible interfaces to work together.
+ -> acts like a "translator" between classes or systems that otherwise couldn't communicate directly.
+
+ Why It is Used:
+  - Compatibility -> Connects code with mismatched interfaces.
+  - Reusability -> Lets you reuse existing classes without modifying them.
+  - Flexibility -> Makes systems more modular by separating adaptation logic.
+  - Integration -> Useful when combining legacy code with new systems or external libraries.
+
+ Intent: 
+  -> Convert the interface of a class into another interface clients expect, so that classes with incompatible 
+     interfaces can work together.
+
+ Use Case:
+  Imagine you're building a data science pipeline: 
+   -> You have a library that outputs results as a NumPy array.
+   -> Another visualization library expects a Python list.
+   -> Instead of rewritting either library, you create an Adapter that converts NumPy arrays inot lists automatically.
+
+ Essentail Elements:
+   1. Target Interface -> The interface your client expects.
+   2. Adaptee -> The existing class with an incompatible interface.
+   3. Adapter -> The "translator" that converts Adaptee's interface into Target's interface.
+   4. Client -> The code that uses the Target interface.
+ 
+"""
