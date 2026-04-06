72deb7c4d3a28c08dcdbc61522488dc063084636
bsparrow49 <aashishchaudhari249@gmail.com>
2026-04-06 22:17:12 +0545

    client code -> Math Engine

---
+++ 04_projects/Design_patterns/Matrix Format Converter/client.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+class MathEngine:
+    def determinant(self,matrix_target):
+        matrix=matrix_target.getmatrix()
+        # Convert to NumPy for calculation
+        arr=np.array(matrix)
+        return round(np.linalg.det(arr),2)
+    
+    def multiply(self,matrix_target_a,matrix_target_b):
+        A = np.array(matrix_target_a.get_matrix())
+        B = np.array(matrix_target_b.get_matrix())
+        result = np.dot(A,B)
+        return result.tolist()
+
